410ec4eb5be443655eae689e39de7566e5db7bff
sksat <sksat@sksat.net>
2019-06-24 13:50:33 +0900

    [add] tiny cat

---
+++ sksat-cat.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define BUF_SIZE	512
+
+int main(int argc, char **argv){
+	char snum;
+	char buf[BUF_SIZE];
+
+	for(;;){
+		snum = read(0, buf, BUF_SIZE);
+		if(snum < 0) break;
+		write(1, buf, snum);
+	}
+}
