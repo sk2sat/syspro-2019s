43abb4065a6a0856b97f4703ba31475343275eca
sksat <sksat@sksat.net>
2019-07-01 11:32:44 +0900

    [add] parse cmd argv

--- sksat-sh.c
+++ sksat-sh.c
@@ -15,30 +15,34 @@
 #define BUF_SIZE	100
 #define MAX_ARGV	10
 
-void get_cmd(char *buf);
-void parse_arg(char *buf, char **argv);
+typedef struct {
+	char *str;
+	int len;
+} String;
+
+char* get_cmdline();
 void exec_cmd(char *buf);
 
 int main(int argc, char **argv){
-	char buf[BUF_SIZE];
-
 	while(true){
 		printf(stdout, "> ");
-		get_cmd(buf);
-		printf(stdout, "cmd: %s\n", buf);
+		char *cmdline = get_cmdline();
+		printf(stdout, "cmdline: %s\n", cmdline);
 
-		exec_cmd(buf);
+		exec_cmd(cmdline);
 	}
 
 	exit();
 }
 
-void get_cmd(char *buf){
+char* get_cmdline(){
+	static char buf[BUF_SIZE];
 	memset(buf, '\0', BUF_SIZE);
 	gets(buf, BUF_SIZE);
 
 	if(buf[0] == EOF)
 		exit();
+	return buf;
 }
 
 bool is_space(const char c){
@@ -47,9 +51,9 @@ bool is_space(const char c){
 	case '\t':
 	case '\n':
 		return true;
+	default:
+		return false;
 	}
-
-	return false;
 }
 
 bool is_delim(const char c){
@@ -65,35 +69,66 @@ bool is_delim(const char c){
 
 char* skip_space(char *p){
 	for(;;){
-		if(is_space(*p) || *p == '\0')
+		if(!is_space(*p) || *p == '\0')
 			break;
 		p++;
 	}
 	return p;
 }
 
-bool is_pipe(char *buf){
+String* get_arg(char *cmd){
+	char *begin = skip_space(cmd);
+	char *end = begin;
+
+	if(*begin == '\0')
+		return NULL;
 	for(;;){
-		if(*buf == '|')
-			return true;
-		else if(*buf == '\0')
-			return false;
-		buf++;
+		if(is_space(*end) || *end=='\0')
+			break;
+		end++;
 	}
+
+	if(cmd == end) return NULL;
+
+	String *arg = malloc(sizeof(String));
+	arg->len = end - cmd;
+	arg->str = malloc(end-begin+1);
+
+	for(int n=0;n<arg->len;n++){
+		printf(stdout, "%c ", arg->str[n]);
+		arg->str[n] = begin[n];
+	}
+	arg->str[arg->len] = '\0';
+
+	return arg;
 }
 
-void exec_cmd(char *buf){
-	char *argv[MAX_ARGV];
+char** parse_arg(char *cmd){
+	static char *argv[MAX_ARGV] = {
+		"",
+	};
+
+	int n=0;
+	for(int argc=1;;argc++){
+		String *a = get_arg(cmd+n);
+		if(a == NULL)
+			break;
+		argv[argc-1] = a->str;
+		n += a->len;
+	}
+
+	return argv;
+}
+
+void exec_cmd(char *cmd){
 	int pid;
 	pid = fork(); // pid=0: child
 
 	if(pid == 0){
-		if(is_pipe(buf)){
-			printf(stdout, "pipe!\n");
-		}
-		parse_arg(buf, argv);
+		char** argv = parse_arg(cmd);
+		printf(stdout, "argv: %s, %s\n", argv[0], argv[1]);
 		exec(argv[0], argv);
 	}
 	wait();
-	printf(stdout, "\nfinished\n");
+	printf(stdout, "\ncmd finished\n");
 }
